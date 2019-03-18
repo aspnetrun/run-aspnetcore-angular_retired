02138e0c3fb1049e49ab9d6aa6d4ece5c8251639
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-03-18 10:32:38 +0300

    base entity class is added

---
+++ src/AspnetRunAngular.Core/Entities/Entity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace AspnetRunAngular.Core.Entities
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateTime { get; set; }
+        public bool IsDeleted { get; set; }
+
+        int? _requestedHashCode;
+
+        public bool IsTransient()
+        {
+            return Id == default(Guid);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Entity))
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            Entity item = (Entity)obj;
+
+            if (item.IsTransient() || IsTransient())
+                return false;
+            else
+                return item.Id == Id;
+        }
+
+        public override int GetHashCode()
+        {
+            if (!IsTransient())
+            {
+                if (!_requestedHashCode.HasValue)
+                    _requestedHashCode = Id.GetHashCode() ^ 31; // XOR for random distribution (http://blogs.msdn.com/b/ericlippert/archive/2011/02/28/guidelines-and-rules-for-gethashcode.aspx)
+
+                return _requestedHashCode.Value;
+            }
+            else
+                return base.GetHashCode();
+
+        }
+        public static bool operator ==(Entity left, Entity right)
+        {
+            if (Equals(left, null))
+                return Equals(right, null) ? true : false;
+            else
+                return left.Equals(right);
+        }
+
+        public static bool operator !=(Entity left, Entity right)
+        {
+            return !(left == right);
+        }
+    }
+}
