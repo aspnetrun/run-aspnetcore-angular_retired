fd9abf1cf48f65d293571a57cc311ef0e7d69ef1
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-03-18 11:04:09 +0300

    CoreException is added

---
+++ src/AspnetRunAngular.Core/Exceptions/CoreException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AspnetRunAngular.Core.Exceptions
+{
+    public class CoreException : Exception
+    {
+        public CoreException()
+        {
+        }
+
+        public CoreException(string message)
+            : base(message)
+        {
+        }
+
+        public CoreException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
