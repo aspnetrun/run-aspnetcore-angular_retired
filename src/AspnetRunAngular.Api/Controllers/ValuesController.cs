5e539517d6bbd4790692c53b7e2486096e01c5f8
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-04-29 14:38:46 +0300

    renamed WebApi project

--- src/AspnetRunAngular.Api/Controllers/ValuesController.cs
+++ src/AspnetRunAngular.Api/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AspnetRunAngular.WebApi.Controllers
+namespace AspnetRunAngular.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- src/AspnetRunAngular.Api/Program.cs
+++ src/AspnetRunAngular.Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace AspnetRunAngular.WebApi
+namespace AspnetRunAngular.Api
 {
     public class Program
     {
