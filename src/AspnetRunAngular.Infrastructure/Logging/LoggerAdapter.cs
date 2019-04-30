8e70f071d29a75302de75f908861d3f90dfa2f16
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-04-30 15:31:31 +0300

    LoggerAdapter implemented

---
+++ src/AspnetRunAngular.Infrastructure/Logging/LoggerAdapter.cs
@@ -0,0 +1,25 @@
+﻿using AspnetRunAngular.Core.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace AspnetRunAngular.Infrastructure.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public LoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+    }
+}
