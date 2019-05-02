d05e763c6a1726ba0fb0675240d78918dd41baad
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-05-02 16:10:09 +0300

    LoggingMiddleware added

--- src/AspnetRunAngular.Api/Startup.cs
+++ src/AspnetRunAngular.Api/Startup.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Reflection;
+using AspnetRunAngular.Api.Application.Middlewares;
 using AspnetRunAngular.Core.Configuration;
 using AspnetRunAngular.Infrastructure.Data;
 using Autofac;
@@ -56,8 +56,6 @@ namespace AspnetRunAngular.Api
             }
 
             app.UseHttpsRedirection();
-
-            app.UseAuthentication();
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
@@ -196,38 +194,4 @@ namespace AspnetRunAngular.Api
             return new AutofacServiceProvider(scope);
         }
     }
-
-
-    //public class Startup
-    //{
-    //    public Startup(IConfiguration configuration)
-    //    {
-    //        Configuration = configuration;
-    //    }
-
-    //    public IConfiguration Configuration { get; }
-
-    //    // This method gets called by the runtime. Use this method to add services to the container.
-    //    public void ConfigureServices(IServiceCollection services)
-    //    {
-    //        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-    //    }
-
-    //    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    //    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    //    {
-    //        if (env.IsDevelopment())
-    //        {
-    //            app.UseDeveloperExceptionPage();
-    //        }
-    //        else
-    //        {
-    //            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    //            app.UseHsts();
-    //        }
-
-    //        app.UseHttpsRedirection();
-    //        app.UseMvc();
-    //    }
-    //}
 }
