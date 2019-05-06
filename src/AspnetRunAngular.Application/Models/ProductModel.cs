b53384482bcab6c91b0252c62a349e22907f9c78
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-05-06 13:05:42 +0300

    bug-fixed

--- src/AspnetRunAngular.Application/Models/ProductModel.cs
+++ src/AspnetRunAngular.Application/Models/ProductModel.cs
@@ -1,4 +1,5 @@
 ﻿using AspnetRunAngular.Application.Models.Base;
+using System;
 
 namespace AspnetRunAngular.Application.Models
 {
@@ -7,7 +8,9 @@ namespace AspnetRunAngular.Application.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal? UnitPrice { get; set; }
+        public Guid CategoryId { get; set; }
         public CategoryModel Category { get; set; }
+        public int StatusId { get; set; }
         public ProductStatusModel Status { get; set; }
     }
 }
