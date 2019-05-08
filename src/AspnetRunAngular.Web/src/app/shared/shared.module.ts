61dc74bc3f64d85914fe5217a3c6c4feff4b80ac
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-05-08 08:51:36 +0300

    Update & Delete modals added

--- src/AspnetRunAngular.Web/src/app/shared/shared.module.ts
+++ src/AspnetRunAngular.Web/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ControlMessageComponent } from './control-messages/control-message.component';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { DataGridComponent } from './data-grid/data-grid.component';
 import { RouterModule } from '@angular/router';
 
@@ -13,11 +14,13 @@ import { RouterModule } from '@angular/router';
     FormsModule,
     RouterModule,
     NgxDatatableModule,
+    ModalModule.forRoot(),
   ],
   exports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    ModalModule,
     ControlMessageComponent,
     DataGridComponent,
   ],
