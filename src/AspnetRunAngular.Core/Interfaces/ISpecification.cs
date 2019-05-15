b87f66db30ab60a9aaf215153c8b82fdb6d7dc15
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-05-15 08:01:15 +0300

    TODO added

--- src/AspnetRunAngular.Core/Interfaces/ISpecification.cs
+++ src/AspnetRunAngular.Core/Interfaces/ISpecification.cs
@@ -7,13 +7,20 @@ namespace AspnetRunAngular.Core.Interfaces
     public interface ISpecification<T>
     {
         Expression<Func<T, bool>> Criteria { get; }
+
         List<Expression<Func<T, object>>> Includes { get; }
+
         List<string> IncludeStrings { get; }
+
         Expression<Func<T, object>> OrderBy { get; }
+
         Expression<Func<T, object>> OrderByDescending { get; }
 
+        // TODO: Paging should be optimized
         int Take { get; }
+
         int Skip { get; }
+
         bool isPagingEnabled { get; }
     }
 }
