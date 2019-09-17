9599c3b64fc5bd406d068f2f90915704a93ebc83
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-09-17 11:20:52 +0300

    adding home route

--- src/AspnetRun.Web/src/app/app-routing.module.ts
+++ src/AspnetRun.Web/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'product', pathMatch: 'full', },
+  { path: 'index', redirectTo: 'product', pathMatch: 'full', },
   { path: 'product', loadChildren: () => import('./views/product/product.module').then(m => m.ProductModule) },
   { path: 'category', loadChildren: () => import('./views/category/category.module').then(m => m.CategoryModule) }
 ];
--- src/AspnetRun.Web/src/app/core/layout/layout.component.ts
+++ src/AspnetRun.Web/src/app/core/layout/layout.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
 
