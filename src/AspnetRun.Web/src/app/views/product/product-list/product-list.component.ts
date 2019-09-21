08b79ce68ef7cc173c96c525fdbd881f4dd69ba0
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-09-21 18:27:59 +0300

    dynamic search added

--- src/AspnetRun.Web/src/app/views/product/product-list/product-list.component.ts
+++ src/AspnetRun.Web/src/app/views/product/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductDataService } from 'src/app/core/services/product-data.service';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/interfaces';
@@ -8,20 +8,33 @@ import { IProduct } from 'src/app/shared/interfaces';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  
   products: IProduct[];
-  productName: string = '';
+  //productName: string = '';
 
-  constructor(private dataService: ProductDataService, route: ActivatedRoute) {
-    route.params.subscribe(() => {
+  _productName: string = '';
+  get productName(): string {
+    return this._productName;
+  }
+  set productName(value: string) {
+    this._productName = value;
+    this.getProducts();
+  }
+
+  constructor(private dataService: ProductDataService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(() => {
       this.getProducts();
     });
   }
 
   getProducts() {
-    this.dataService.getProductsByName(this.productName)
-      .subscribe((products: IProduct[]) => {
+    this.dataService.getProductsByName(this.productName).subscribe((products: IProduct[]) => {
         this.products = products;
       });
   }
+
 }
